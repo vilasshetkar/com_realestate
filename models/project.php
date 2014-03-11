094813d691b9a404687ebf57fb9e772ee82e91ab
vilasshetkar <vilasshetkar@gmail.com>
2014-03-11 12:02:44 +0530

    IP database host name

--- models/project.php
+++ models/project.php
@@ -27,7 +27,7 @@ class RealEstateModelProject extends JModelItem
                 $option = array(); //prevent problems
  
 				$option['driver']   = 'mysql';            // Database driver name
-				$option['host']     = 'localhost';    // Database host name 169.254.182.25
+				$option['host']     = '169.254.182.25';    // Database host name 169.254.182.25
 				$option['user']     = 'yogesh';       // User for database authentication
 				$option['password'] = 'root';   // Password for database authentication
 				$option['database'] = 'prop';      // Database name
