59bc285a4a52ad72d2a2c31a14b0393d7b88d997
vilasshetkar <vilasshetkar@gmail.com>
2014-04-14 22:43:37 +0530

    Home Project Module for Single project home page
    Home Project Module for Single project home page

---
+++ Modules/mod_homeproject/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+
+<div class="row-fluid home_project">
+<div class="span3">
+  <ul class="nav menu proj_nav">
+      <li class="item-134"><a href="/index.php?option=com_content&amp;view=article&amp;id=15&amp;Itemid=134">Overview</a></li>
+      <li class="item-136"><a href="/index.php?option=com_content&amp;view=article&amp;id=19&amp;Itemid=136">Area Statement</a></li>
+      <li class="item-137"><a href="/index.php?option=com_content&amp;view=article&amp;id=18&amp;Itemid=137">Floor Plan</a></li>
+      <li class="item-135"><a href="/index.php?option=com_content&amp;view=article&amp;id=16&amp;Itemid=135">Amenities Specification</a></li>
+      <li class="item-138"><a href="/index.php?option=com_content&amp;view=article&amp;id=20&amp;Itemid=138">Layout &amp; Location Map</a></li>
+      <li class="item-139"><a href="/index.php?option=com_content&amp;view=article&amp;id=17&amp;Itemid=139">Walk Through</a></li>
+    </ul>
+</div>
+<div class="span6">
+<?php echo $hello["title"]; ?>
+</div>
+<div class="span3">
+</div>
+
+</div>
+
