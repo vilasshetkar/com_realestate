e0bb011514c67d845a71e571e8ecb92331bbcd78
vilasshetkar <vilasshetkar@gmail.com>
2014-04-18 08:57:15 +0530

    Module template changed in jaiprakash's office
    Module template changed in jaiprakash's office

--- Modules/mod_homeproject/tmpl/default.php
+++ Modules/mod_homeproject/tmpl/default.php
@@ -5,11 +5,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 <div class="span3">
   <ul class="nav menu proj_nav">
       <li class="item-134"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Overview</a></li>
-      <li class="item-136"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Area Statement</a></li>
-      <li class="item-137"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Floor Plan</a></li>
-      <li class="item-135"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Amenities Specification</a></li>
-      <li class="item-138"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Layout &amp; Location Map</a></li>
-      <li class="item-139"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=overview&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Walk Through</a></li>
+      <li class="item-136"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=area-statement&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Area Statement</a></li>
+      <li class="item-137"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=floor-plan&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Floor Plan</a></li>
+      <li class="item-135"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=amenities-specification&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Amenities Specification</a></li>
+      <li class="item-138"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=location-layout&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Layout &amp; Location Map</a></li>
+      <li class="item-139"><a href="index.php?option=com_realestate&amp;view=project&amp;layout=walk-through&amp;id=<?php echo $hello["id"]; ?>&amp;project=<?php echo $hello["title"]; ?>">Walk Through</a></li>
     </ul>
 </div>
 <div class="span6">
